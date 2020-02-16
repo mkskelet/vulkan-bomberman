490b2baeb9c8efc37b0b05947a35c4d2353b2a26
Marek Kost <mk.skelet@gmail.com>
2020-02-16 21:06:06 +0100

    Say logger refactor

--- vulkan-bomberman/Say.h
+++ vulkan-bomberman/Say.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <type_traits>
 
+enum LogType {Log, Info, Warning, Error};
+
 class Say
 {
 private:
@@ -31,18 +33,43 @@ public:
         Log(vars...);
     };
 
+    template <typename... Types>
+    static void Log(LogType logType, Types... vars)
+    {
+        switch (logType)
+        {
+        case Info:
+            std::cout << "INFO: ";
+            break;
+        case Warning:
+            std::cout << "WARNING: ";
+            break;
+        case Error:
+            std::cout << "ERROR: ";
+            break;
+        default:
+            break;
+        }
+
+        Log(vars...);
+    };
+
     template <typename T, typename... Types>
     static void LogError(T var, Types... vars)
     {
-        std::cout << "ERROR: ";
-        Log(var, vars...);
+        Log(Error, var, vars...);
     };
 
     template <typename T, typename... Types>
     static void LogWarning(T var, Types... vars)
     {
-        std::cout << "WARNING: ";
-        Log(var, vars...);
+        Log(Warning, var, vars...);
+    };
+
+    template <typename T, typename... Types>
+    static void LogInfo(T var, Types... vars)
+    {
+        Log(Info, var, vars...);
     };
 
     // conditional logging
@@ -70,6 +97,15 @@ public:
         if (condition)
         {
             LogWarning(vars...);
+        }
+    };
+
+    template <typename... Types>
+    static void LogInfoIf(bool condition, Types... vars)
+    {
+        if (condition)
+        {
+            LogInfo(vars...);
         }
     };
 };
